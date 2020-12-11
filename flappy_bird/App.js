ef0218a6eab557f2f2cc89172b1d9757588d48f4
Heronnes Pereira <heronnestg@hotmail.com>
2020-12-11 11:48:37 -0300

    parte 3

--- flappy_bird/App.js
+++ flappy_bird/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View, Dimensions } from 'react-native'
 import Bird from './components/Bird.js'
 
@@ -6,7 +6,22 @@ import Bird from './components/Bird.js'
 export default function App() {
     const larguraTela = Dimensions.get('screen').width
     const alturaTela = Dimensions.get('screen').height
-    console.log(larguraTela, alturaTela)
+    //console.log(larguraTela, alturaTela)
+
+    const esquerdoDoPassaro = larguraTela / 2
+    const [fundoDoPassaro, setFundoDoPassaro] = useState(alturaTela/2)
+    const gravidade = 3
+    
+    //iniciar pássaro caindo
+    useEffect(() => {
+      if (fundoDoPassaro > 0) {
+        setInterval(() => {
+          setFundoDoPassaro(fundoDoPassaro => fundoDoPassaro - gravidade)
+        }, 1000)
+      }
+    })
+    
+    //console.log(fundoDoPassaro)
 
     return (
       <View style={estilos.container}>
